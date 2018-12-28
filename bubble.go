190d7cee78f796989a29c61da210b13d036bfd58
marciodasilva <marciodasilva@hotmail.com>
2018-12-28 15:21:09 -0800

    Adding Descending order to sort

--- bubble.go
+++ bubble.go
@@ -2,34 +2,53 @@ package main
 
 import "fmt"
 
+const ASC = 1
+const DESC = -1
+
 func main() {
-	var numbers []int = []int{5, 4, 2, 3, 1, 0}
-	fmt.Println("List to be sorted", numbers)
-	bubbleSort(numbers)
-	fmt.Println("Sorted list...", numbers)
+	var numbers []int = []int{0, 6, 5, 3, 2, 1, 4}
+	bubbleSortInt(numbers, ASC)
+	fmt.Println("List to be sorted in Ascending Order", numbers)
+	bubbleSortInt(numbers, DESC)
+	fmt.Println("List to be sorted in Descending Order", numbers)
 }
 
-func bubbleSort(numbers []int) {
+func bubbleSortInt(numbers []int, order int) {
 	var N int = len(numbers)
 	for i := 0; i < N; i++ {
-		sweep(numbers, i)
+		if !sweep(numbers, i, order) {
+			return
+		}
 		fmt.Println(numbers)
 	}
 }
 
-func sweep(numbers []int, iterations int) {
+func sweep(numbers []int, iterations int, order int) bool {
 	var N = len(numbers)
 	firstIndex := 0
 	secondIndex := 1
+	swapped := false
 	for secondIndex < (N - iterations) {
 		var firstNumber int = numbers[firstIndex]
 		var secondNumber int = numbers[secondIndex]
 		// swapping
-		if firstNumber > secondNumber {
-			numbers[firstIndex] = secondNumber
-			numbers[secondIndex] = firstNumber
+		if order > 0 {
+			if firstNumber > secondNumber {
+				numbers[firstIndex] = secondNumber
+				numbers[secondIndex] = firstNumber
+				swapped = true
+			}
+
+		} else {
+			if firstNumber < secondNumber {
+				numbers[firstIndex] = secondNumber
+				numbers[secondIndex] = firstNumber
+				swapped = true
+			}
+
 		}
 		firstIndex++
 		secondIndex++
 	}
+	return swapped
 }
